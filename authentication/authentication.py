c106755c02e8a3397e0c40c7be069d3b8b969ab3
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2017-02-25 02:13:41 +0530

    Update authentication.py

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -13,35 +13,16 @@ def installation_fun():
     if request.method == 'POST':
         if request.form['submit'] == 'Save':
             fi = open('ks.cfg', 'w')
-            install_new_installation = request.form['install_new_installation']
-            install_source = request.form['install_source']
-            install_source_server = request.form['install_source_server']
-            install_source_dict = request.form['install_source_dict']
-            install_check_ftp = "false"
-            install_ftp_user = request.form['install_ftp_user']
-            install_ftp_pass = request.form['install_ftp_pass']
-            if request.form.get("install_check_ftp"):
-                install_check_ftp = "True"
-            install_new_installation = install_new_installation.split("$$")
-            fi.write("\n" + install_new_installation[0] + "\n" + install_new_installation[1])
-            if install_source == "# Use CDROM installation media $$cdrom":
-                cdrom = install_source.split("$$")
-                fi.write("\n" + cdrom[0] + "\n" + cdrom[1])
-            elif install_source == "# Use NFS installation media $$nfs":
-                nfs = install_source.split("$$")
-                fi.write("\n" + nfs[0] + "\n" + "nfs --server=" + install_source_server + " --dir=" + install_source_dict)
-            elif install_source == "# Use network installation $$http":
-                http = install_source.split("$$")
-                fi.write("\n" + http[0] + "\nurl --url=\"http://" + install_source_server + "/" + install_source_dict + "\"")
-            elif install_source == "# Use network installation $$ftp":
-                ftp = install_source.split("$$")
-                if install_check_ftp == "false":
-                    fi.write("\n" + ftp[0] + "\nurl --url=\"ftp://" + install_source_server + "/" + install_source_dict + "\"")
-                if install_check_ftp == "True":
-                    fi.write("\n" + ftp[0] + "\nurl --url=\"ftp://" + install_ftp_user + ":" + install_ftp_pass + "@" + install_source_server + "/" + install_source_dict + "\"")
-            elif install_source == "# Use hard drive installation media $$hard_drive":
-                hard_drive = install_source.split("$$")
-                fi.write("\n" + hard_drive[0] + "\nharddrive --dir=" + install_source_server + " --partition=" + install_source_dict + "\"")
+            auth_line2 = "auth"
+            if request.form.get("auth_shadow_pass"):
+                auth_line2 += " --useshadow"
+            auth_security_algo = request.form['auth_security_algo']
+            auth_line2 = auth_line2 + auth_security_algo
+            if request.form.get("auth_finger_reader"):
+                auth_line2 += " --enablefingerprint"
+            if request.form.get("auth_enable_nis"):
+                    auth_line2 += " --enablenis --nisdomain=" + request.form['auth_nis_domain']
+            fi.write("\n" + "# System authorization information\n" + auth_line2)
             fi.close()
             return render_template('auth.html')
     elif request.method == 'GET':
@@ -50,4 +31,3 @@ def installation_fun():
 
 if __name__ == '__main__':
     app.run()
-
